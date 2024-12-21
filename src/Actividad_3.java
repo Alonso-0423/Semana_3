dbc10ce1e040ccf1b992c723c3086d80bece7dcd
Alonso <alonso.pinzon@uip.edu.pa>
2024-12-20 22:21:28 -0500

    Actividad_3

---
+++ src/Actividad_3.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class Actividad_3 {
+    public static void main(String[] args) {
+        //cualquier cosa
+
+        /* cualquier cosa
+           y otras cosas  */
+
+        short numeroEntero = 1;
+        byte numeroEntero2 = 5;
+        int numeroEntero3 = 10; //usen esto
+        long numeroEntero4 = 100;
+
+        float numerodecimal = 3.14f;
+        double numerodecimal2 = 3.14; // usen este
+
+        char letras = 'A';
+        String texto = "hablen, pelaos";
+
+        boolean esVerdadero = true;
+
+
+        // operador Matematico
+        int suma = numeroEntero + 37;
+        System.out.println("el resutado es " + suma);
+
+
+        // lectura por teclado
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("numero Entero: ");
+        numeroEntero = scanner.nextShort();
+        System.out.print("El numero leido es " + numeroEntero);
+
+    }
+}
